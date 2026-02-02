e72309de1a8264cbcb6c082f970737a2095e92bd
Elisabetta Piacquadio <betta.pcq@gmail.com>
2026-02-02 17:46:52 +0100

    add corrections

--- src/main/java/bettapiac/exu2w1d1/Exu2w1d1Application.java
+++ src/main/java/bettapiac/exu2w1d1/Exu2w1d1Application.java
@@ -1,5 +1,6 @@
 package bettapiac.exu2w1d1;
 
+import bettapiac.exu2w1d1.entities.Menu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -9,10 +10,12 @@ public class Exu2w1d1Application {
 
 
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(Exu2w1d1Application.class);
-        SpringApplication.run(Exu2w1d1Application.class, args);
 
-        Men
+        SpringApplication.run(Exu2w1d1Application.class, args);
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(Exu2w1d1Application.class);
+
+        Menu menu = ctx.getBean(Menu.class);
+        menu.printMenu();
     }
 }
 
