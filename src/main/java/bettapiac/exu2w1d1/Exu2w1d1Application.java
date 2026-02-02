d3087cd9dcce8e4b14a73ad5df882ce290695238
Elisabetta Piacquadio <betta.pcq@gmail.com>
2026-02-02 17:29:29 +0100

    update

--- src/main/java/bettapiac/exu2w1d1/Exu2w1d1Application.java
+++ src/main/java/bettapiac/exu2w1d1/Exu2w1d1Application.java
@@ -2,15 +2,18 @@ package bettapiac.exu2w1d1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class Exu2w1d1Application {
 
+
     public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(Exu2w1d1Application.class);
         SpringApplication.run(Exu2w1d1Application.class, args);
+
+        Men
     }
-
-
 }
 
 
